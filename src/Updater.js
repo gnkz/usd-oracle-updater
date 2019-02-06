9c0645d1a926430fda6e566a703e66a5296efc5d
gnkz <gnkz@users.noreply.github.com>
2019-02-06 16:39:00 -0300

    Refactor updater

--- src/Updater.js
+++ src/Updater.js
@@ -1,38 +1,62 @@
 const { utils } = require("ethers");
 
 const Updater = (provider, wallet, address) => {
+  // Create a contract instance
   const contract = new utils.Interface(abi);
 
   return async (usdValue) => {
+    // 1 eth = 10^18 wei
     const oneEth = utils.bigNumberify("0xde0b6b3a7640000");
+
+    // rate = 10^18/usdValue
+    // Decimals are not allowed
     const rate = oneEth.div(Math.floor(usdValue));
 
-    const noncePromise = provider.getTransactionCount(wallet.address);
-    const gasPricePromise = provider.getGasPrice();
+    // Get the data payload to call the updateRate function
     const data = contract.functions["updateRate"].encode([rate.toHexString()]);
 
+    // Obtain the chain id
+    const chainId = provider.chainId;
+
+    // Create a minimal transaction to estimate the gas limit
+    const minimalTx = {
+      to: address,
+      data,
+      chainId,
+    };
+
+    // Create a promise to estimate the gas limit
+    const gasLimitPromise = provider.estimateGas({
+      from: wallet.address,
+      ...minimalTx,
+    });
+
+    // Create a promise to obtain the account nonce
+    const noncePromise = provider.getTransactionCount(wallet.address);
+
+    // Create a promise to obtain the gas price from the network
+    const gasPricePromise = provider.getGasPrice();
+
+    // Obtain the values from the promises
+    const gasLimit = await gasLimitPromise;
     const nonce = await noncePromise;
     const gasPrice = await gasPricePromise;
 
-    let tx = {
+    // Create a full transaction
+    const tx = {
       nonce,
       gasPrice,
-      data,
-      to: address,
-      chainId: 3,
+      gasLimit,
+      ...minimalTx,
     };
 
-    const gasLimit = await provider.estimateGas({
-      from: wallet.address,
-      ...tx,
-    });
-
-    tx.gasLimit = gasLimit;
-
+    // Sign the transaction using the wallet private key
     const signedTx = await wallet.sign(tx);
 
+    // Send the transaction and obtain the transaction hash
     const { hash } = await provider.sendTransaction(signedTx);
 
+    // Wait till the transaction is mined
     await provider.waitForTransaction(hash);
 
     return hash;
